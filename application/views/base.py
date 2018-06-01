dc5a921f0f5b18b7b9d94ecc1823d3b09f457f43
rylandos <ryland.karlovich@informed.com>
2018-06-01 17:37:32 +0100

    Added several form element types and a test implementation using json

--- application/views/base.py
+++ application/views/base.py
@@ -2,10 +2,13 @@
 from django.shortcuts import render
 
 
-def render_form(request, form):
+def render_form(request, elements, form_heading):
     if request.method == 'GET':
+        for element in elements:
+            element['type'] = "./form-elements/" + element['type'] + ".html"
+
         # Get the blank form
-        return render(request, 'form.html', {'form': form})
+        return render(request, 'form.html', {'elements': elements, 'form_heading': form_heading})
     else:
         # For now, render a generic success page
         return render(request, 'success.html')
