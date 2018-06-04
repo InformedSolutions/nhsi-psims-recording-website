655a7707ecf9215e0df0ca9a9a0617572b419145
rylandos <ryland.karlovich@informed.com>
2018-06-04 15:13:11 +0100

    Added additional form element types and validation in html

--- application/views/base.py
+++ application/views/base.py
@@ -5,10 +5,17 @@ from django.shortcuts import render
 def render_form(request, elements, form_heading):
     if request.method == 'GET':
         for element in elements:
+
+            # For fieldsets recurse through sub-elements to set the partial path
+            if element['type'] == 'fieldset':
+                for e in element['elements']:
+                    e['type'] = "./form-elements/" + e['type'] + ".html"
+
+            # Set the correct file path for the partial that will render the element
             element['type'] = "./form-elements/" + element['type'] + ".html"
 
         # Get the blank form
-        return render(request, 'form.html', {'elements': elements, 'form_heading': form_heading})
+        return render(request, 'form.html', {'elements': elements, 'form_heading': form_heading, 'submit_text': 'Submit'})
     else:
         # For now, render a generic success page
         return render(request, 'success.html')
